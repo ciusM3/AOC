7e16d30b2ac15d7fa4479d635b9fcb44b3ada494
Iustin <ciobicaiustin2@gmail.com>
2025-12-10 01:02:15 +0200

    Day 9

--- Advent of code 2025/AOC_D9.cpp
+++ Advent of code 2025/AOC_D9.cpp
@@ -1,6 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-
 #include <iostream>
 #include <fstream>
 #include <cstdio>
@@ -16,7 +15,40 @@ typedef struct redCoords
 {
 	int x;
 	int y;
+	int xOg, yOg;
 };
+
+void dfsFillExterior(char map[300][300], int mapSize, int x, int y)
+{
+	if (x < 0 || x >= mapSize || y < 0 || y >= mapSize)
+		return;
+
+	if (map[x][y] != '.')
+		return;
+
+	map[x][y] = 'O';
+
+	dfsFillExterior(map, mapSize, x + 1, y);
+	dfsFillExterior(map, mapSize, x - 1, y);
+	dfsFillExterior(map, mapSize, x, y + 1);
+	dfsFillExterior(map, mapSize, x, y - 1);
+}
+
+void fillExterior(char map[300][300], int mapSize)
+{
+	// flood fill from all sides
+	for (int i = 0; i < mapSize; i++)
+	{
+		if (map[0][i] == '.') 
+			dfsFillExterior(map, mapSize, 0, i);
+		if (map[mapSize - 1][i] == '.') 
+			dfsFillExterior(map, mapSize, mapSize - 1, i);
+		if (map[i][0] == '.') 
+			dfsFillExterior(map, mapSize, i, 0);
+		if (map[i][mapSize - 1] == '.') 
+			dfsFillExterior(map, mapSize, i, mapSize - 1);
+	}
+}
 
 void sortXValues(redCoords* redPositions, int noPositions)
 {
@@ -122,7 +154,7 @@ void part1()
 				maxSurfArea = surfArea;
 		}
 
-	cout << maxSurfArea;
+	cout << "answer: " << maxSurfArea;
 }
 
 void part2()
@@ -131,14 +163,14 @@ void part2()
 
 	char line[100];
 	redCoords redPositions[500];
-	redCoords redPositionsBig[500];
+
 	int indexPos = 0;     // no of elements from input
 
 	while (input >> line)
 	{
 		sscanf(line, "%d,%d", &redPositions[indexPos].y, &redPositions[indexPos].x);
-		redPositionsBig[indexPos].y = redPositions[indexPos].y;
-		redPositionsBig[indexPos].x = redPositions[indexPos].x;
+		redPositions[indexPos].xOg = redPositions[indexPos].x;
+		redPositions[indexPos].yOg = redPositions[indexPos].y;
 		indexPos++;
 	}
 
@@ -158,12 +190,13 @@ void part2()
 
 	cout << endl;
 
+	// create blank map
 	for (int i = 0; i < mapSize; i++)
 	{
 		for (int j = 0; j < mapSize; j++)
 			map[i][j] = '.';
 	}
-	
+
 	// create the borders 
 	for (int i = 0; i < indexPos; i++)
 	{
@@ -178,7 +211,7 @@ void part2()
 		redCoords C = redPositions[i - 1];
 
 		int smallest, biggest;
-		
+
 		if (A.y == C.y)
 		{
 			smallest = MIN(A.x, C.x) + 1;
@@ -191,41 +224,100 @@ void part2()
 	}
 
 	// fill for horizontal
-	for (int row = 0; row < mapSize; row++)
+	for (int i = 0; i < mapSize; i++)
 	{
 		int first = -1;
 		int last = -1;
 
-		for (int col = 0; col < mapSize; col++)
+		for (int j = 0; j < mapSize; j++)
 		{
-			if (map[row][col] == '#')
+			if (map[i][j] == '#')
 			{
 				if (first == -1)
-					first = col;
-				last = col;
+					first = j;
+				last = j;
 			}
 		}
 
 		if (first != -1 && last != -1 && first < last)
 		{
-			for (int col = first + 1; col < last; col++)
+			for (int j = first + 1; j < last; j++)
 			{
-				if (map[row][col] == '.')
-					map[row][col] = 'X';
+				if (map[i][j] == '.')
+					map[i][j] = 'X';
 			}
 		}
 	}
 
-	// fill out
+	fillExterior(map, mapSize);
 
-
-
+	// print map
 	for (int i = 0; i < mapSize; i++)
 	{
 		for (int j = 0; j < mapSize; j++)
 			cout << map[i][j];
 		cout << endl;
 	}
+
+	long long int maxSurfArea = -1;
+
+	int x1, x2, y1, y2;
+
+	for (int i = 0; i < indexPos - 1; i++)
+		for (int j = i + 1; j < indexPos; j++)
+		{
+			redCoords A = redPositions[i];
+			redCoords C = redPositions[j];
+			redCoords B = { C.x, A.y };
+			redCoords D = { A.x, C.y };
+
+			//see if rectangle is inside by following the edge of the current rectangle, if in its path there is O it means its not fully inside => skip
+			int rectangleInside = 1;
+
+			int smallestX, biggestX;
+			int smallestY, biggestY;
+
+			// edge A->B
+			smallestX = MIN(A.x, B.x);
+			biggestX = MAX(A.x, B.x);
+			for (int i = smallestX; i <= biggestX && rectangleInside != 0; i++)
+				if (map[i][A.y] == 'O')
+					rectangleInside = 0;
+			
+			// edge C->D
+			smallestX = MIN(C.x, D.x);
+			biggestX = MAX(C.x, D.x);
+			for (int i = smallestX; i <= biggestX && rectangleInside != 0; i++)
+				if (map[i][C.y] == 'O')
+					rectangleInside = 0;
+
+			// edge A->D
+			smallestY = MIN(A.y, B.y);
+			biggestY = MAX(A.y, B.y);
+			for (int i = smallestY; i <= biggestY && rectangleInside != 0; i++)
+				if (map[A.x][i] == 'O')
+					rectangleInside = 0;
+
+			// edge B->C
+			smallestY = MIN(B.y, C.y);
+			biggestY = MAX(B.y, C.y);
+			for (int i = smallestY; i <= biggestY && rectangleInside != 0; i++)
+				if (map[C.x][i] == 'O')
+					rectangleInside = 0;
+
+			if (rectangleInside == 0)
+				continue;
+			
+			long long int xDif = abs(A.xOg - C.xOg) + 1;
+			long long int yDif = abs(A.yOg - C.yOg) + 1;
+
+			long long int surfArea = xDif * yDif;
+
+			if (surfArea > maxSurfArea)
+					maxSurfArea = surfArea;
+		}
+
+	cout << endl << "answer: " << maxSurfArea;
 }
 
 int main()
